12ed03d9f9eab0f02e21afd917872f72684baf30
leehh <leehh@leehhs-MacBook-Air.local>
2017-11-13 20:59:34 -0500

    hough.py finished

--- code/plot_hough_lines.py
+++ code/plot_hough_lines.py
@@ -24,16 +24,16 @@ def plot_hough_lines(lines,img):
 
 def check_hough_lines():
     if not os.path.isdir('../ref_data/pics_with_hough_lines'):
-        print ('calculating hough lines...')
         return False
     else:
         print ('pics with hough lines already exits')
-        return True if input('If recalculation needed, please enter \'y\',\
-         that would induce overwritten,\n else enter \'n\' for just loading:\n') == 'y' else False
+        return False if input('If recalculation needed, please enter \'y\',that would induce overwritten,else enter \'n\' for just loading:\n')\
+                       == 'y' else True
 
 def load_and_show():
+    print ('loading img...')
     for i in range(1, 201):
-        new_img = cv2.imread('../original_pics/' + str(i).zfill(3) + '.tif')
+        new_img = cv2.imread('../ref_data/pics_with_hough_lines/pics_lines_' + str(i).zfill(3) + '.tif')
         cv2.imshow('img', new_img)
         # press ESC to exit
         if cv2.waitKey(200) & 0xff == 27:
@@ -41,12 +41,17 @@ def load_and_show():
 
 def main():
     if not check_hough_lines():
+        print ('calculating hough lines...')
         for i in range(1,201):
             img = cv2.imread('../original_pics/'+str(i).zfill(3)+'.tif')
             lines = hough_lines(img)
             new_img = plot_hough_lines(lines,img)
             hough_lines_only = plot_hough_lines(lines,np.zeros((img.shape[0],img.shape[1],3)))
+            if not os.path.exists('../ref_data/pics_with_hough_lines'):
+                os.mkdir('../ref_data/pics_with_hough_lines')
             cv2.imwrite('../ref_data/pics_with_hough_lines/pics_lines_' + str(i).zfill(3) + '.tif', new_img)
+            if not os.path.exists('../ref_data/hough_lines_only'):
+                os.mkdir('../ref_data/hough_lines_only')
             cv2.imwrite('../ref_data/hough_lines_only/lines_only_' + str(i).zfill(3) + '.tif', hough_lines_only)
             cv2.imshow('img', new_img)
             # press ESC to exit
