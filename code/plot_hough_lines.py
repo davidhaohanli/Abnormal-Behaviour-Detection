0d2881b984b7bfa09e18718b2647c3ccef2114ff
leehh <leehh@leehhs-MacBook-Air.local>
2017-11-13 20:36:57 -0500

    hough_lines_plot.py updated

--- code/plot_hough_lines.py
+++ code/plot_hough_lines.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 def hough_lines(img):
     return cv2.HoughLines(cv2.Canny(img,105,130),1,np.pi/180,89)
@@ -21,17 +22,38 @@ def plot_hough_lines(lines,img):
         print ('Hough line not find')
     return img;
 
-def main():
-    for i in range(1,201):
-        img = cv2.imread('../original_pics/'+str(i).zfill(3)+'.tif')
-        lines = hough_lines(img)
-        new_img = plot_hough_lines(lines,img)
-        #new_img = plot_hough_lines(lines,np.zeros((img.shape[0],img.shape[1],3)))
+def check_hough_lines():
+    if not os.path.isdir('../ref_data/pics_with_hough_lines'):
+        print ('calculating hough lines...')
+        return False
+    else:
+        print ('pics with hough lines already exits')
+        return True if input('If recalculation needed, please enter \'y\',\
+         that would induce overwritten,\n else enter \'n\' for just loading:\n') == 'y' else False
+
+def load_and_show():
+    for i in range(1, 201):
+        new_img = cv2.imread('../original_pics/' + str(i).zfill(3) + '.tif')
         cv2.imshow('img', new_img)
         # press ESC to exit
-        cv2.imwrite('../ref_data/pics_with_hough_lines/h_lines_only_'+str(i).zfill(3)+'.tif',new_img)
         if cv2.waitKey(200) & 0xff == 27:
-            break
+            break;
+
+def main():
+    if not check_hough_lines():
+        for i in range(1,201):
+            img = cv2.imread('../original_pics/'+str(i).zfill(3)+'.tif')
+            lines = hough_lines(img)
+            new_img = plot_hough_lines(lines,img)
+            hough_lines_only = plot_hough_lines(lines,np.zeros((img.shape[0],img.shape[1],3)))
+            cv2.imwrite('../ref_data/pics_with_hough_lines/pics_lines_' + str(i).zfill(3) + '.tif', new_img)
+            cv2.imwrite('../ref_data/hough_lines_only/lines_only_' + str(i).zfill(3) + '.tif', hough_lines_only)
+            cv2.imshow('img', new_img)
+            # press ESC to exit
+            if cv2.waitKey(200) & 0xff == 27:
+                break;
+    else:
+        load_and_show()
 
 if __name__ == '__main__':
     main();
