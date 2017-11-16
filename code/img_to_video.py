714d2ab1213b38f40980a9216cdad10dd7736b70
leehh <leehh@leehhs-MacBook-Air.local>
2017-11-15 21:10:37 -0500

    added video

---
+++ code/img_to_video.py
@@ -0,0 +1,40 @@
+def make_video(images, outvid=None, fps=5, size=None,
+               is_color=True, format="XVID"):
+    """
+    Create a video from a list of images.
+
+    @param      outvid      output video
+    @param      images      list of images to use in the video
+    @param      fps         frame per second
+    @param      size        size of each frame
+    @param      is_color    color
+    @param      format      see http://www.fourcc.org/codecs.php
+    @return                 see http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+
+    The function relies on http://opencv-python-tutroals.readthedocs.org/en/latest/.
+    By default, the video will have the size of the first image.
+    It will resize every image to this size before adding them to the video.
+    """
+    from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
+    import os
+    fourcc = VideoWriter_fourcc(*format)
+    vid = None
+    for image in images:
+        if not os.path.exists(image):
+            raise FileNotFoundError(image)
+        img = imread(image)
+        if vid is None:
+            if size is None:
+                size = img.shape[1], img.shape[0]
+            vid = VideoWriter(outvid, fourcc, float(fps), size, is_color)
+        if size[0] != img.shape[1] and size[1] != img.shape[0]:
+            img = resize(img, size)
+        vid.write(img)
+    vid.release()
+    return vid
+
+if __name__ == '__main__':
+    images=[]
+    for i in range(1,201):
+        images.append('../original_pics/'+str(i).zfill(3)+'.tif')
+    make_video(images,'../video/video.mp4')
