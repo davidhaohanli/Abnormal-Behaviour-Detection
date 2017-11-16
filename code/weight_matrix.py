4a38868f8baf5be6809ca665aa854eca6f79a2f1
leehh <leehh@leehhs-MacBook-Air.local>
2017-11-15 19:04:49 -0500

    weight_matrix

--- code/weight_matrix.py
+++ code/weight_matrix.py
@@ -2,23 +2,24 @@ import re
 import numpy as np
 from xmlLoader_generator import *
 
-def diff(pic):
-    res = []
-    if pic is not None:
-        for y in pic:
-            res.append((int(y.get('val')), abs(int(re.findall('\(.*,', y[0].text)[0][1:-1]) - \
-                                               int(re.findall('\(.*,', y[-1].text)[0][1:-1]))))
-    return res;
-
 class Weight_matrix:
 
-    def __init__(self,tps=diff(Poi_handle().searchPic(3))):
+    def __init__(self,n=3):
+        tps=self.diff(Poi_handle().searchPic(n))
         self.y1 = tps[0][0]
         self.y2 = tps[-1][0]
         self.ab = tps[0][1];
         self.cd = tps[-1][1];
         #print (self.y1)
         self.compute_weight_matrix()
+
+    def diff(self,pic):
+        res = []
+        if pic is not None:
+            for y in pic:
+                res.append((int(y.get('val')), abs(int(re.findall('\(.*,', y[0].text)[0][1:-1]) - \
+                                                   int(re.findall('\(.*,', y[-1].text)[0][1:-1]))))
+        return res;
 
     def y_weight(self,y):
         return (y - self.y2) / (self.y1 - self.y2) + (self.y1 - y) / (self.y1 - self.y2) * (self.ab / self.cd)
@@ -34,7 +35,7 @@ def test ():
     if n == 'd':
         print (Weight_matrix().get_weight_matrix())
     else:
-        print(Weight_matrix(diff(Poi_handle().searchPic(n))).get_weight_matrix())
+        print(Weight_matrix(n).get_weight_matrix())
 
 if __name__ == '__main__':
     test()
