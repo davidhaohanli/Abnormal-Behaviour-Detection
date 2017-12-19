9d08c14f46cc74408f2bed85cdd94982b611f1be
leehh <lihaohandavid@gmail.com>
2017-12-18 19:35:05 -0500

    comment for core

--- code/core.py
+++ code/core.py
@@ -13,9 +13,9 @@ def uvPlot(u,v,labels,timerSet=False):
     fig=plt.figure()
     for ind,label in enumerate(labels):
         if label:
-            plt.scatter(u[ind],v[ind],c='red')
+            plt.scatter(u[ind],v[ind],c='red')#red for abnormal person
         else:
-            plt.scatter(u[ind], v[ind], c='blue')
+            plt.scatter(u[ind], v[ind], c='blue')#blue for walking people
     if timerSet:
         timer = fig.canvas.new_timer(interval=10000)  # creating a timer object and setting an interval of 3000 milliseconds
         timer.add_callback(plt.close)
@@ -27,23 +27,24 @@ def uvPlot(u,v,labels,timerSet=False):
     plt.show()
 
 def load_data():
+    #optical flow (u v) data generated from matlab script
     u_seq_abnormal = scipy.io.loadmat('../ref_data/u_seq_abnormal.mat')['u_seq_abnormal']
     v_seq_abnormal = scipy.io.loadmat('../ref_data/v_seq_abnormal.mat')['v_seq_abnormal']
 
-    fg_imgs = ['../ref_data/fg_pics/' + str(i + 1) + '.bmp' for i in range(200)]
-    original_imgs = ['../ref_data/original_pics/' + str(i + 1).zfill(3) + '.tif' for i in range(200)]
-    abnormal_fg_imgs = ['../ref_data/ab_fg_pics/' + str(i + 1) + '.bmp' for i in range(200)]
+    fg_imgs = ['../ref_data/fg_pics/' + str(i + 1) + '.bmp' for i in range(200)]#foreground extracted images
+    original_imgs = ['../ref_data/original_pics/' + str(i + 1).zfill(3) + '.tif' for i in range(200)]#original images
+    abnormal_fg_imgs = ['../ref_data/ab_fg_pics/' + str(i + 1) + '.bmp' for i in range(200)]#abnormal person position by manually drawing
 
     return u_seq_abnormal,v_seq_abnormal,fg_imgs,original_imgs,abnormal_fg_imgs
 
 def plot(realPos,labels,img,classifier,timerSet=True):
     target=[None,0]
     for i, item in enumerate(realPos):
-        if labels[i]:
+        if labels[i]:#consider only abnormal people (label 1)
             score= item[-1]/((item[0]-item[1])*(item[2]-item[3]))
-            if score >= target[-1]:
+            if score >= target[-1]:#the largest position
                 target=[item,score]
-    if target[-1]:
+    if target[-1]:#if no abnormal person detected, tag nothing
         item=target[0]
         cv2.rectangle(img, (int(item[3]), int(item[1])), (int(item[2]), int(item[0])), (0, 0, 255))
     cv2.imshow('classifier: {}'.format(classifier), img)
@@ -56,33 +57,34 @@ def plot(realPos,labels,img,classifier,timerSet=True):
 
 def main ():
 
-    u_data,v_data,fg_imgs,original_imgs,abnormal_fg_imgs=load_data()
-    weight = Weight_matrix().get_weight_matrix()
+    u_data,v_data,fg_imgs,original_imgs,abnormal_fg_imgs=load_data()#load data from ref_data
+
+    weight = Weight_matrix().get_weight_matrix()#use normalization method for feature correction
 
     thisFeatureExtractor = Feature_extractor(original_imgs,fg_imgs,abnormal_fg_imgs,u_data,v_data,weight)
 
-    train_data,train_labels = thisFeatureExtractor.get_features_and_labels(80,140)
+    train_data,train_labels = thisFeatureExtractor.get_features_and_labels(80,140)#training frames
 
-    ########################## To see the features distribution, uncomment next line##################################
+    ########################## To see the train data features distribution, uncomment next line##################################
     #uvPlot(train_data[:,0],train_data[:,1],train_labels,False)
-    ##################################################################################################################
+    #############################################################################################################################
 
     classifiers = Classifiers(train_data,train_labels)
 
-    test_data, test_labels = thisFeatureExtractor.get_features_and_labels(140,199)
+    test_data, test_labels = thisFeatureExtractor.get_features_and_labels(140,199)#testing frames
 
-    for name,model in classifiers.models.items():
-        for ind,original_img in enumerate(original_imgs[:-1]):
+    for name,model in classifiers.models.items():#get each classifier
+        for ind,original_img in enumerate(original_imgs[:-1]):#get each frame
 
-            pos,thisImg,_,_=thisFeatureExtractor.getPosition(fg_imgs,ind)
+            pos,thisImg,_,_=thisFeatureExtractor.getPosition(fg_imgs,ind)#get the position of each person in this frame
 
-            features,_=thisFeatureExtractor.get_features_and_labels(ind,ind+1,False)
+            features,_=thisFeatureExtractor.get_features_and_labels(ind,ind+1,False)#get the features for each person in the frame
 
-            labels=classifiers.models[name].predict(features)
+            labels=classifiers.models[name].predict(features)#predict label
 
-            plot(pos,labels,thisImg,name)
+            plot(pos,labels,thisImg,name)#show
 
-        classifiers.prediction_metrics(test_data,test_labels,name)
+        classifiers.prediction_metrics(test_data,test_labels,name)#metrics for each classifier based on the test data
 
 if __name__ == '__main__':
     print (
